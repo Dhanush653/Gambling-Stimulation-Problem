0f2ad921977cf5a51138d26906465aeb0482243f
Dhanush Kumar <dk652003@gmail.com>
2024-01-25 18:11:22 +0530

    Total Profit Or Loss After 20 Days

--- src/com/bridgelabz/gamblingstimulation/Gambling.java
+++ src/com/bridgelabz/gamblingstimulation/Gambling.java
@@ -1,28 +1,38 @@
 package com.bridgelabz.gamblingstimulation;
 
 import java.util.Random;
-public class Gambling {
+public class Gambling
+{
 
-    int Stack_Per_Day = 100;
-    int Bet_Every_Game = 1;
-    int Days = 20;
+    private int Stack_Per_Day = 100;
+    private int Bet_Every_Game = 1;
+    private int Days = 20;
 
 
-    public void wonorloss(){
+    public void wonorloss()
+    {
         Random random = new Random();
         int Random_Win_Loss;
         int Probablity_Amount;
-        for(int i =1;i<21;i++){
+        int Total_Amound_Win = 0;
+        int Total_Amount_Loss = 0;
+        for(int i =1;i<=Days;i++){
             Random_Win_Loss = random.nextInt(2);
-            Probablity_Amount = random.nextInt(100);
+            Probablity_Amount = random.nextInt(Stack_Per_Day);
             if(Random_Win_Loss == 1){
-
-                System.out.println("Day "+i+": "+"Profit of "+Probablity_Amount);
+                Total_Amound_Win += Probablity_Amount;
             }
             else{
-                System.out.println("Day "+i+": "+"Loss of "+Probablity_Amount);
+                Total_Amount_Loss += Probablity_Amount;
             }
 
+        }
+        int Total_amount = (Total_Amound_Win-Total_Amount_Loss);
+        if(Total_amount > 0){
+            System.out.println("The Total Profit is " + Total_amount);
+        }
+        else{
+            System.out.println("The Total Loss is " + Total_amount);
         }
     }
 
