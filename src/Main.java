e8b9aa22d80340ccf81dcde1f226df631aafdc99
Dhanush Kumar <dk652003@gmail.com>
2024-01-25 11:58:05 +0530

    Added Win or Lose Probility

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args)
     {
         Gambling ob1 = new Gambling();
-        ob1.probablity();
+        ob1.limit();
+
 
     }
 }
--- src/com/bridgelabz/gamblingstimulation/Gambling.java
+++ src/com/bridgelabz/gamblingstimulation/Gambling.java
@@ -5,16 +5,20 @@ public class Gambling {
 
     int Stack_Per_Day = 100;
     int Bet_Every_Game = 1;
-
-    public void probablity(){
-        Random random = new Random();
-        int a = random.nextInt(2);
-        if (a == 1){
-            System.out.println("The Gambler Won the Stack");
+    public void limit(){
+        int No_Of_Stock = 40;
+        int Maximum_Loose_limit = Stack_Per_Day/2;
+        int Maximum_Win_limit = Stack_Per_Day + (50/100);
+        if (No_Of_Stock>=Maximum_Loose_limit){
+            System.out.println("Resign Gambling");
+        }
+        else if (No_Of_Stock <=Maximum_Win_limit) {
+            System.out.println("Resign Gambling");
         }
         else{
-            System.out.println("The Gambler Lose The Stack");
+            System.out.println("Continue Gambling");
         }
+
     }
 
 }
