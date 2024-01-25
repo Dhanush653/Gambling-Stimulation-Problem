6a42c0e59cb46a148e3ac79a30994c707bbae05d
Dhanush Kumar <dk652003@gmail.com>
2024-01-25 12:57:43 +0530

    Win or Loss For 20 Days

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import com.bridgelabz.gamblingstimulation.Gambling;
 public class Main {
     public static void main(String[] args)
     {
+        Gambling ob1 = new Gambling();
+        ob1.wonorloss();
+
 
     }
 }
